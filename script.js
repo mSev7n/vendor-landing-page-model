eba7681b9e76be39c89dbb58e2da5f9b2d9b6f1b
mSev7n <pythagorasm7@gmail.com>
2026-04-13 14:00:57 +0100

    defined products in script

---
+++ script.js
@@ -0,0 +1,12 @@
+const products = [
+  { id: 1, name: "Straight lace front", price: 45000, img: "👱‍♀️" },
+  { id: 2, name: "Curly bob",            price: 38000, img: "👩‍🦱" },
+  { id: 3, name: 'Body wave 18"',        price: 52000, img: "💁‍♀️" },
+  { id: 4, name: "Kinky coily",          price: 35000, img: "👩‍🦳" },
+  { id: 5, name: "Blonde highlight",     price: 60000, img: "🧖‍♀️" },
+  { id: 6, name: "Sleek pixie",          price: 28000, img: "🙎‍♀️" },
+];
+
+// NOTE!!!!!!: when you have real photos, set img to the file path:
+// for example img: "images/straight-lace.jpg"
+
