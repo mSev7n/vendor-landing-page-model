6c73a734be47bcb9f183c257be577f5f75653d88
mSev7n <pythagorasm7@gmail.com>
2026-04-13 14:03:22 +0100

    defined the cart and created two helper functions, one to format numbers in naira and another to count total items in cart

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ const products = [
 // NOTE!!!!!!: when you have real photos, set img to the file path:
 // for example img: "images/straight-lace.jpg"
 
+const cart = {};
+
+function fmt(n) {
+  return "₦" + n.toLocaleString("en-NG");
+}
+
+function totalItems() {
+  return Object.values(cart).reduce((sum, qty) => sum + qty, 0);
+}
+
+function totalPrice() {
+  return Object.keys(cart).reduce((sum, id) => {
+    const p = products.find(x => x.id == id);
+    return sum + p.price * cart[id];
+  }, 0);
+}
+
